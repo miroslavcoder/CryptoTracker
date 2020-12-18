1bb0237926d33109f86d588df01d9c26e84cd754
miroslavcoder <miroslavphoto@gmail.com>
2020-12-18 22:29:30 +0000

    Initial commit

---
+++ investment.js
@@ -0,0 +1,24 @@
+
+//Balace: Is the total amount of the alt-coin that you own (for example 10000 QTUM)
+//Initial_Buying: Is the amount you bought with your Currency (for example you payed 1.5 ETH)
+//currency: Is the currency you paid to buy your alt coin
+const investment = 
+{
+  'TRX':{
+    balance: 25874.10000000,
+    initial_buying: 1.53846000,
+    currency: 'ETH',
+  },
+  'XRP':{
+    balance: 3190.80600000,
+    initial_buying: 4.99861000,
+    currency: 'ETH',
+   },
+  'VET':{
+    balance: 12987.00000000,
+    initial_buying: 0.35100000,
+    currency: 'ETH',
+  }
+};
+
+module.exports = investment;
